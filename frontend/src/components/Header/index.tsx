b3ddbe752795205847e186cbd35d26a62ee490a1
vog10 <vog10@github.com>
2022-07-12 15:52:19 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import logo from  '../../assets/img/logo.svg'
+
+import  './styles.css'
+
+function Header() {
+    return (
+    
+        <header>
+        <div className="dsmeta-logo-container">
+            <img src={logo} alt="DSMeta"/>
+            <h1>DSMeta</h1>
+            <p>
+                Desenvolvido por
+                <a href="https://github.com/Vog10" >
+                     @Vambaster Gomes
+                </a>
+            </p>
+
+        </div>
+
+    </header>
+    )
+
+}
+
+export default Header
