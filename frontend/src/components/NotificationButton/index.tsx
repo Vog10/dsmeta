559ff6c205cf7fde8cad450a82041349a14a9c89
vog10 <vog10@github.com>
2022-07-16 13:48:43 -0300

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,15 +1,31 @@
-import icon from  '../../assets/img/notification-icon.svg';
+import axios from 'axios';
+import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 
 import './styles.css';
 
-function NotificationButton() {
-    return (
-        
-            <div className="dsmeta-red-btn">
-                <img src={icon} alt="Notificar" />
-            </div>
 
-       
+type Props = {
+    saleId: number;
+}
+
+
+function handleClick(id: number) {
+    axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response =>{
+        console.log("SUCESSO");
+    } )
+
+}
+
+function NotificationButton({ saleId }: Props) {
+    return (
+
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
+            <img src={icon} alt="Notificar" />
+        </div>
+
+
     )
 
 }
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -26,17 +26,16 @@ function SalesCard() {
     useEffect(() => {
 
 
-       const dmin = minDate.toISOString().slice(0,10);
-       const dmax = maxDate.toISOString().slice(0,10);
+        const dmin = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
 
 
-       console.log(dmin);
 
         axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 setSales(response.data.content);
             })
-    }, [minDate,maxDate]);
+    }, [minDate, maxDate]);
 
 
     return (
@@ -89,7 +88,7 @@ function SalesCard() {
                                     <td>R$ {sale.amount.toFixed(2)}</td>
                                     <td>
                                         <div className="dsmeta-red-btn-container">
-                                            <NotificationButton />
+                                            <NotificationButton saleId={sale.id} />
                                         </div>
                                     </td>
                                 </tr>
